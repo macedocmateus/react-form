bc75ed5ff6d2f2a27f7c7d4a643e62233823d33a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-17 06:07:40 -0300

    add onsubmit and onchange and creating prevent default into form

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
-import "./App.css"
+import "./App.css";
+
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div>
-      <h1>Evento</h1>
+    const [name, setName] = useState("");
 
-      <form>
-        <input type="text" placeholder="Nome do evento" />
-        <span className="error">Nome é obrigatório</span>
+    function onSubmit(e: React.FormEvent<HTMLElement>) {
+        e.preventDefault();
+    }
 
-        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+    return (
+        <div>
+            <h1>Evento {name}</h1>
 
-        <select defaultValue="">
-          <option value="" disabled>
-            Selecione...
-          </option>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nome do evento"
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <span className="error">Nome é obrigatório</span>
 
-          <option value="technology">React</option>
-          <option value="entertainment">Node.js</option>
-          <option value="business">Javascript</option>
-          <option value="business">Typescript</option>
-        </select>
+                <input type="date" placeholder="Nome do evento" lang="pt-BR" />
 
-        <textarea placeholder="Descrição" rows={4} />
+                <select defaultValue="">
+                    <option value="" disabled>
+                        Selecione...
+                    </option>
 
-        <button type="submit">Salvar</button>
-      </form>
-    </div>
-  )
+                    <option value="technology">React</option>
+                    <option value="entertainment">Node.js</option>
+                    <option value="business">Javascript</option>
+                    <option value="business">Typescript</option>
+                </select>
+
+                <textarea placeholder="Descrição" rows={4} />
+
+                <button type="submit">Salvar</button>
+            </form>
+        </div>
+    );
 }
